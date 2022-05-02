5d0a040aabba4b4fe8ce9249397f74fcff98d065
Augustin LORAIN <alorain@e2r7p21.clusters.42paris.fr>
2022-05-02 19:28:35 +0200

    start of ex02 of module00

---
+++ module00/ex02/Account.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alorain <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/02 19:16:34 by alorain           #+#    #+#             */
+/*   Updated: 2022/05/02 19:24:51 by alorain          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account( int initial_deposit );
+{
+}
+
+Account::~Account( void );
+{
+}
+
+
+static int	Account::getNbAccounts( void );
+{
+}
+
+static int	Account::getTotalAmount( void );
+{
+}
+
+static int	Account::getNbDeposits( void );
+{
+}
+
+static int	Account::getNbWithdrawals( void );
+{
+}
+
+static void	Account::displayAccountsInfos( void );
+{
+}
+
+
+
+void	Account::makeDeposit( int deposit );
+{
+}
+
+bool	Account::makeWithdrawal( int withdrawal );
+{
+}
+
+int		Account::checkAmount( void ) const;
+{
+}
+
+void	Account::displayStatus( void ) const;
+{
+}
+
+static void	Account::_displayTimestamp( void );
+{
+}
