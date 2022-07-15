c038857adf415bea9c756d8d941bc1a045a57cf2
GusFiveO <augustinlorain@gmail.com>
2022-07-15 16:09:20 +0200

    module06 ex02 done

--- module06/ex02/include/Base.hpp
+++ module06/ex02/include/Base.hpp
@@ -6,7 +6,7 @@
 /*   By: alorain <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 17:39:50 by alorain           #+#    #+#             */
-/*   Updated: 2022/07/14 17:49:15 by alorain          ###   ########.fr       */
+/*   Updated: 2022/07/15 15:47:01 by augustinlorai    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,25 @@
 
 class Base {
 	public:
-		virtual ~Base(void);
+		virtual ~Base(void) { std::cout << "Base destructor" << std::endl;};
 };
 
 class A : public Base {
+	public:
+		A(void) { std::cout << "A constructor" << std::endl;};
+		~A(void) { std::cout << "A destructor" << std::endl;};
 };
 
 class B : public Base {
+	public:
+		B(void) { std::cout << "B constructor" << std::endl;};
+		~B(void) { std::cout << "B destructor" << std::endl;};
 };
 
 class C : public Base {
+	public:
+		C(void) { std::cout << "C constructor" << std::endl;};
+		~C(void) { std::cout << "C destructor" << std::endl;};
 };
 
 #endif
--- module06/ex02/src/main.cpp
+++ module06/ex02/src/main.cpp
@@ -6,32 +6,128 @@
 /*   By: alorain <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 17:49:30 by alorain           #+#    #+#             */
-/*   Updated: 2022/07/14 18:01:38 by alorain          ###   ########.fr       */
+/*   Updated: 2022/07/15 16:08:46 by augustinlorai    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
 #include <cstdlib>
+#include <time.h>
 
 Base *generate(void)
 {
-	int	rabdNum = rand() % 3;
+	int	randNum = rand() % 3;
 	switch (randNum)
 	{
 		case 0:
 		{
-			return ;
+			Base * a = new A();
+			return a;
 		}
 		case 1:
 		{
-			return ;
+			Base * b = new B();
+			return b;
 		}
 		case 2:
 		{
-			return ;
+			Base * c = new C();
+			return c;
 		}
 	}
+	return NULL;
+}
+
+bool isClass(Base & ptr, char base)
+{
+	if (base == 'A')
+	{
+		try
+		{
+			Base &temp = dynamic_cast<A&>(ptr);
+			(void)temp;
+		}
+		catch (std::bad_cast & e)
+		{
+			return false;
+		}
+	}
+	else if (base == 'B')
+	{
+		try
+		{
+			Base &temp = dynamic_cast<B&>(ptr);
+			(void)temp;
+		}
+		catch (std::bad_cast & e)
+		{
+			return false;
+		}
+	}
+	else if (base == 'C')
+	{
+		try
+		{
+			Base &temp = dynamic_cast<C&>(ptr);
+			(void)temp;
+		}
+		catch (std::bad_cast & e)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void identify(Base & p)
+{
+	if (isClass(p, 'A'))
+			std::cout << "A" << std::endl;
+	else if (isClass(p, 'B'))
+			std::cout << "B" << std::endl;
+	else if (isClass(p, 'C'))
+			std::cout << "C" << std::endl;
+}
+
+bool isClass(Base * ptr, char base)
+{
+	Base * temp;
+	if (base == 'A')
+	{
+		temp = dynamic_cast<A*>(ptr);
+		if (!temp)
+			return false;
+	}
+	else if (base == 'B')
+	{
+		temp = dynamic_cast<B*>(ptr);
+		if (!temp)
+			return false;
+	}
+	else if (base == 'C')
+	{
+		temp = dynamic_cast<C*>(ptr);
+		if (!temp)
+			return false;
+	}
+	return true;
+}
+
+void identify(Base * p)
+{
+	if (isClass(p, 'A'))
+			std::cout << "A" << std::endl;
+	else if (isClass(p, 'B'))
+			std::cout << "B" << std::endl;
+	else if (isClass(p, 'C'))
+			std::cout << "C" << std::endl;
 }
 
 int main (void) {
+	srand(time(0));
+	Base * randClass = generate();
+	Base & ref = *randClass;
+	identify(randClass);
+	identify(ref);
+	delete randClass;
 }
