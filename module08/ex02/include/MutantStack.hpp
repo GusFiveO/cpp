97f388d3d2097ea9299e55b97cd03e5022274ce4
Augustin LORAIN <alorain@e1r9p12.clusters.42paris.fr>
2022-07-19 17:22:15 +0200

    finish the module08 ex02

--- module08/ex02/include/MutantStack.hpp
+++ module08/ex02/include/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: alorain <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 19:55:03 by alorain           #+#    #+#             */
-/*   Updated: 2022/07/18 20:17:30 by alorain          ###   ########.fr       */
+/*   Updated: 2022/07/19 17:21:08 by alorain          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,21 @@
 # define MUTANTSTACK_HPP
 
 # include <iostream>
+# include <vector>
+# include <stack>
+# include <deque>
 
-class MutantStack
+template <typename T, class Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container> {
+	public:
+		MutantStack(void) {}
+		MutantStack(MutantStack<T> & copy) : std::stack<T, Container>(copy) {}
+		MutantStack<T> & operator=(MutantStack<T> & assign);
+
+		typedef typename std::stack<T, Container>::container_type::iterator iterator;
+
+		iterator begin(void) {return std::stack<T, Container>::c.begin();}
+		iterator end(void) {return std::stack<T, Container>::c.end();}
+};
 
 #endif
---
+++ module08/ex02/src/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alorain <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/19 13:34:18 by alorain           #+#    #+#             */
+/*   Updated: 2022/07/19 16:20:41 by alorain          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+#include <iostream>
+#include <vector>
+#include <stack>
+
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
